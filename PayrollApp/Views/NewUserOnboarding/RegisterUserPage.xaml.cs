e57ea774d09195f045f8e795ef754ae139ddb86a
Erwin Suwitoandojo <erwin.suwitoandojo@outlook.com>
2020-03-13 19:07:24 +0700

    Added boilerplate code to do login

--- PayrollApp/Views/NewUserOnboarding/RegisterUserPage.xaml.cs
+++ PayrollApp/Views/NewUserOnboarding/RegisterUserPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PayrollCore.Entities;
+﻿using Microsoft.Toolkit.Graph.Providers;
+using PayrollCore.Entities;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -56,7 +57,50 @@ namespace PayrollApp.Views.NewUserOnboarding
             timeUpdater.Tick += TimeUpdater_Tick;
             timeUpdater.Start();
 
+            loadTimer.Interval = new TimeSpan(0, 0, 1);
+            loadTimer.Tick += LoadTimer_Tick;
+            loadTimer.Start();
+
             pageTitle.Text = upn;
+        }
+
+        private async void LoadTimer_Tick(object sender, object e)
+        {
+            loadTimer.Stop();
+            bool IsUserEnabled = false;
+            if (upn != null)
+            {
+                User user = await SettingsHelper.Instance.da.GetUserFromDbById(upn);
+                if (user != null)
+                {
+                    pageTitle.Text = user.fullName;
+
+                    bool IsUserADEnabled = await IsUserEnabledAD(upn);
+                    if (IsUserADEnabled == true)
+                    {
+                        // Do login
+                    }
+                    else
+                    {
+                        IsUserEnabled = false;
+                    }
+                }
+                else
+                {
+                    progText.Text = "Please wait. We're setting up your account...";
+                    // Do account setup
+                }
+
+                if (IsUserEnabled == false)
+                {
+                    await ShowAccountDisabledMessage();
+                    this.Frame.Navigate(typeof(LoginPage), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromLeft });
+                }
+            }
+            else
+            {
+                this.Frame.Navigate(typeof(LoginPage), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromLeft });
+            }
         }
 
         private void TimeUpdater_Tick(object sender, object e)
@@ -65,5 +109,59 @@ namespace PayrollApp.Views.NewUserOnboarding
             currentDate.Text = DateTime.Now.ToString("dd/MM/yyyy");
         }
 
+        private async Task<bool> IsUserEnabledAD(string upn)
+        {
+            var provider = ProviderManager.Instance.GlobalProvider;
+
+            if (provider != null && provider.State == ProviderState.SignedIn)
+            {
+                try
+                {
+                    var user = await provider.Graph.Users[upn].Request().GetAsync();
+                    if (user != null)
+                    {
+                        if (user.AccountEnabled == true)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                catch (Microsoft.Graph.ServiceException graphEx)
+                {
+                    if (graphEx.Message.Contains("Request_ResourceNotFound"))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Graph Service Exception: " + graphEx.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Check user AD Exception: " + ex.Message);
+                }
+            }
+
+            return false;
+        }
+
+        private async Task<bool> ShowAccountDisabledMessage()
+        {
+            ContentDialog contentDialog = new ContentDialog
+            {
+                Title = "Your account is disabled.",
+                Content = "If you believe that your account has been disabled by mistake, contact TA Supervisor, Chiefs, or TA HR Functional Unit to enable your account.",
+                PrimaryButtonText = "Ok"
+            };
+
+            await contentDialog.ShowAsync();
+
+            return true;
+        }
     }
 }
