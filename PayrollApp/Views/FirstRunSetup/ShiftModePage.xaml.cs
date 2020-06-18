2f5a272669770e21fed33de43c09015d7ecce876
Erwin Suwitoandojo <erwin.suwitoandojo@outlook.com>
2020-06-18 09:48:03 +0700

    Added page to toggle shift on or off

---
+++ PayrollApp/Views/FirstRunSetup/ShiftModePage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PayrollApp.Views.FirstRunSetup
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PermissionsPage : Page
+    {
+        public PermissionsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        DispatcherTimer timeUpdater = new DispatcherTimer();
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            currentTime.Text = DateTime.Now.ToString("hh:mm tt");
+            currentDate.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            timeUpdater.Interval = new TimeSpan(0, 0, 30);
+            timeUpdater.Tick += TimeUpdater_Tick;
+            timeUpdater.Start();
+        }
+
+        private void TimeUpdater_Tick(object sender, object e)
+        {
+            currentTime.Text = DateTime.Now.ToString("hh:mm tt");
+            currentDate.Text = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        private async void nextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CameraTestDialog testDialog = new CameraTestDialog();
+            ContentDialogResult result = await testDialog.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+                CameraTipsDialog tipsDialog = new CameraTipsDialog();
+                await tipsDialog.ShowAsync();
+            }
+            else if (result == ContentDialogResult.Secondary)
+            {
+                this.Frame.Navigate(typeof(GraphSetupPage), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromRight });
+            }
+        }
+    }
+}
