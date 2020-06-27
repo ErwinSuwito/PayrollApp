ce7a3509dd89ccbbe5ef0658e765aff76e8819a7
Erwin Suwitoandojo <erwin.suwitoandojo@outlook.com>
2020-06-27 10:53:11 +0700

    Added PersonalizationPage and removed ReviewGlobalSettingsPage

--- PayrollApp/Views/FirstRunSetup/PersonalizationPage.xaml.cs
+++ PayrollApp/Views/FirstRunSetup/PersonalizationPage.xaml.cs
@@ -21,9 +21,9 @@ namespace PayrollApp.Views.FirstRunSetup
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ReviewGlobalSettingsPage : Page
+    public sealed partial class PersonalizationPage : Page
     {
-        public ReviewGlobalSettingsPage()
+        public PersonalizationPage()
         {
             this.InitializeComponent();
         }
