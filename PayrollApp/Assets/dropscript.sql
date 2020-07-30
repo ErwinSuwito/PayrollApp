8de1c34baee74ae8e1e45198c16e0cf1746c17d9
Erwin Suwitoandojo <erwin.suwitoandojo@outlook.com>
2020-07-30 10:00:47 +0700

    Updated script to drop tables

--- PayrollApp/Assets/dropscript.sql
+++ PayrollApp/Assets/dropscript.sql
@@ -1,9 +1,23 @@
-DROP TABLE IF EXISTS Global_Settings;
-DROP TABLE IF EXISTS Activity;
-DROP TABLE IF EXISTS Meeting_Group;
-DROP TABLE IF EXISTS Meeting;
-DROP TABLE IF EXISTS Shifts;
-DROP TABLE IF EXISTS Location;
-DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS user_group;
-DROP TABLE IF EXISTS Rate;
+while(exists(select 1 from INFORMATION_SCHEMA.TABLE_CONSTRAINTS where CONSTRAINT_TYPE='FOREIGN KEY'))
+begin
+ declare @sql nvarchar(2000)
+ SELECT TOP 1 @sql=('ALTER TABLE ' + TABLE_SCHEMA + '.[' + TABLE_NAME
+ + '] DROP CONSTRAINT [' + CONSTRAINT_NAME + ']')
+ FROM information_schema.table_constraints
+ WHERE CONSTRAINT_TYPE = 'FOREIGN KEY'
+ exec (@sql)
+ PRINT @sql
+end
+
+while(exists(select 1 from INFORMATION_SCHEMA.TABLES 
+             where TABLE_NAME != '__MigrationHistory' 
+             AND TABLE_TYPE = 'BASE TABLE'))
+begin
+ SELECT TOP 1 @sql=('DROP TABLE ' + TABLE_SCHEMA + '.[' + TABLE_NAME
+ + ']')
+ FROM INFORMATION_SCHEMA.TABLES
+ WHERE TABLE_NAME != '__MigrationHistory' AND TABLE_TYPE = 'BASE TABLE'
+exec (@sql)
+ /* you dont need this line, it just shows what was executed */
+ PRINT @sql
+end
